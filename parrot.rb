6c6caafc45029255e5bd112e04e2714140bb2910
beautiful-instructions-5625 <slen.detablan@gmail.com>
2020-06-15 22:25:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
+def phrase("Pretty bird!")
   parrot = "Squawk!"
-  puts "Pretty bird!"
+  puts "Squawk!"
   return "Squawk!"
 end
   
